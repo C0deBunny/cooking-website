dcad61840cfb1a6d58e6ac2fab559155c40b76bb
Usagi <denzel.9@live.nl>
2026-03-13 22:09:58 +0100

    feat: update Home component layout and add a button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>test content</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <Button>Test Button</Button>
+    </div>
+  );
 }
