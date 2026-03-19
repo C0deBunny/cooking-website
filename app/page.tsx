ea994c2093850eff209a5d4fa2c1c9fbc957a613
Usagi <denzel.9@live.nl>
2026-03-19 19:22:01 +0100

    feat: refactor Home page and add HeroSection component for enhanced layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,16 @@
-"use client";
-
-// import lib
-import { createClient } from "@/lib/supabase/client";
-import { useState, useEffect } from "react";
-
 // import components
-import HeroSection from "@/components/feature/layout/hero/hero";
-
-//import types
-import { Recipes } from "@/types/recipes";
+import HeroSection from "@/components/feature/layout/hero/heroSection";
 
 export default function Home() {
-  const [recipes, setRecipes] = useState<Recipes>([]);
-
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      const { data, error } = await createClient().from("recipes").select("*");
-
-      if (error) {
-        console.error("Error fetching recipes:", error);
-        return;
-      }
-
-      if (data) {
-        setRecipes(data);
-      }
-    };
-
-    fetchRecipes();
-  }, []);
-
   return (
     <div>
       <HeroSection />
+
+      {/* TODO Featured Recipes */}
+
+      {/* TODO New Recipes */}
+
+      {/* TODO: About me */}
     </div>
   );
 }
